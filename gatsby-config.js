92e5018631c3ed6065a6834ee3a2c724de94e1df
aurimas-darguzis <aurimas@darguzis.com>
2019-07-02 20:35:55 +0100

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `your_space_id_grab_it_from_contentful`,
+        accessToken: `your_token_id_grab_it_from_contentful`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
